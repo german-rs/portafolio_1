0e1c33b4ef1f411d6b7dc69adb8507b10a02e4ac
german-rs <ing.germanriveros@gmail.com>
2024-09-17 14:22:22 -0300

    Mejora de accesibilidad, navegacion por teclado y se agrega foco

--- src/utils/mobile-nav.js
+++ src/utils/mobile-nav.js
@@ -1,8 +1,10 @@
 const mobileNav = () => {
 
+    
     const headerBtn = document.querySelector('.header__bars');
     const mobileNav = document.querySelector('.mobile-nav');
     const mobileLinks = document.querySelectorAll('.mobile-nav__link');
+    
 
     //State.
     let isMobileNavOpen = false;
@@ -13,9 +15,12 @@ const mobileNav = () => {
         if (isMobileNavOpen) {
             mobileNav.style.display = 'flex';
             document.body.style.overflowY = 'hidden';
+            
+            
         } else {
             mobileNav.style.display = 'none';
             document.body.style.overflowY = 'auto';
+            
         }
     });
 
